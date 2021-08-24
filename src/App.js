c4b70b6e83779133bc279d9ad3d6908e2925b2a4
milan-48 <mp890520@gmail.com>
2021-08-24 13:38:49 +0530

    updated prebuilt version , remove token and meeting code generation api calls, added participantCanLeave flag

--- src/App.js
+++ src/App.js
@@ -1,80 +1,46 @@
 import React, { useEffect } from "react";
-import { VideoSDKMeeting } from "@videosdk.live/js-prebuilt";
+import { VideoSDKMeeting } from "@videosdk.live/rtc-js-prebuilt";
 
 export default function App() {
-  useEffect(async () => {
-    const token = await getToken();
-    const meetingId = await getMeetingId(token);
-    if (meetingId) {
-      let name = "Demo User";
-      const videoMeetingSpecs = {
-        micEnabled: true,
-        webcamEnabled: true,
-        name,
-        meetingId,
-        redirectOnLeave: window.location.href,
-        chatEnabled: true,
-        screenShareEnabled: true,
-        pollEnabled: true,
-        whiteBoardEnabled: true,
-        participantCanToggleSelfWebcam: true,
-        participantCanToggleSelfMic: true,
-        raiseHandEnabled: true,
-        token: token,
-        containerId: null,
-        recordingEnabled: true,
-        recordingWebhookUrl: "https://www.videosdk.live/callback",
-        recordingEnabledByDefault: false,
-        participantCanToggleRecording: true,
-        brandingEnabled: true,
-        brandLogoURL:
-          "https://app.videosdk.live/_next/image?url=%2Fvideosdk_logo_circle.png&w=1920&q=75",
-        brandName: "VIDEO SDK LIVE",
-      };
-      const videoMeeting = new VideoSDKMeeting();
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_VIDEOSDK_API_KEY;
+    const meetingId = "milkyway";
+    const name = "Demo User";
 
-      await videoMeeting.init(videoMeetingSpecs);
-    }
+    const config = {
+      name: name,
+      meetingId: meetingId,
+      apiKey: apiKey,
+
+      containerId: null,
+      redirectOnLeave: "https://www.videosdk.live/",
+
+      micEnabled: true,
+      webcamEnabled: true,
+      participantCanToggleSelfWebcam: true,
+      participantCanToggleSelfMic: true,
+
+      chatEnabled: true,
+      screenShareEnabled: true,
+      pollEnabled: true,
+      whiteBoardEnabled: true,
+      raiseHandEnabled: true,
+
+      recordingEnabled: true,
+      recordingEnabledByDefault: false,
+      recordingWebhookUrl: "https://www.videosdk.live/callback",
+      participantCanToggleRecording: true,
+
+      brandingEnabled: true,
+      brandLogoURL: "https://picsum.photos/200",
+      brandName: "Awesome startup",
+
+      participantCanLeave: false, // if false, leave button won't be visible
+    };
+
+    const meeting = new VideoSDKMeeting();
+    meeting.init(config);
   }, []);
 
-  const getToken = async () => {
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_VIDEOSDK_API_ENDPOINT}/get-token`,
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const { token } = await response.json();
-      return token;
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  const getMeetingId = async (token) => {
-    try {
-      const VIDEOSDK_API_ENDPOINT = `${process.env.REACT_APP_VIDEOSDK_API_ENDPOINT}/create-meeting`;
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ token }),
-      };
-      const response = await fetch(VIDEOSDK_API_ENDPOINT, options)
-        .then(async (result) => {
-          const { meetingId } = await result.json();
-          return meetingId;
-        })
-        .catch((error) => console.log("error", error));
-      return response;
-    } catch (e) {
-      console.log(e);
-    }
-  };
   return <div></div>;
 }

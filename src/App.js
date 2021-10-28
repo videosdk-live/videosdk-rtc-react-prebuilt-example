7c79254af68d7ac091687557ab21f63beffbb70e
ChintanRajpara <chintan@zujo.io>
2021-10-28 11:35:30 +0530

    pin feature added in example

--- src/App.js
+++ src/App.js
@@ -52,6 +52,11 @@ export default function App() {
         toggleParticipantMic: true, // Can toggle other participant's mic
         toggleParticipantWebcam: true, // Can toggle other participant's webcam
       },
+
+      pin: {
+        allowed: true, // participant can pin any participant in meeting
+        layout: "SPOTLIGHT", // meeting layout - GRID | SPOTLIGHT | SIDEBAR
+      },
     };
 
     const meeting = new VideoSDKMeeting();
